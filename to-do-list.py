ae07fa670fb07568312512a12a98cd3723eed84e
Frank Yan <frankyan53@gmail.com>
2025-05-14 18:24:56 -0700

    Add JSON load/save functionality

--- to-do-list.py
+++ to-do-list.py
@@ -1,1 +1,13 @@
-print("To-do list test.")
+import json
+import os
+
+
+def load_tasks():
+    with open("tasks.json", "r") as f:
+        tasks = json.load(f)
+        return tasks
+
+
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f, indent=2)
