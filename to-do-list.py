7d5e504903aec9ae6fb523f0e12dadd8f40eeb2f
Frank Yan <frankyan53@gmail.com>
2025-05-15 18:31:37 -0700

    feat: add list_tasks() for formatted task view with format_task_name() helper for name truncation

--- to-do-list.py
+++ to-do-list.py
@@ -55,6 +55,10 @@ def archive_task(task):
     save_archived_tasks(archived_tasks)
 
 
+def format_task_name(task_name):
+    return f"{task_name[:14]}..." if len(task_name) > 17 else task_name
+
+
 def add_task():
     tasks = load_current_tasks()
     id_counter = load_id_counter()
@@ -104,3 +108,45 @@ def delete_task():
     task = choose_task(tasks)
     tasks.remove(task)
     save_current_tasks(tasks)
+    print("Task deleted successfully.")
+
+
+def list_tasks():
+    current_tasks = load_current_tasks()
+    archived_tasks = load_archived_tasks()
+    task_labels_dict = {
+        "1": "LIST OF CURRENT TASKS",
+        "2": "LIST OF TO DO TASKS",
+        "3": "LIST OF IN PROGRESS TASKS",
+        "4": "LIST OF COMPLETED TASKS",
+        "5": "LIST OF ARCHIVED TASKS"
+    }
+    task_list_dict = {
+        "1": current_tasks,
+        "2": [task for task in current_tasks if task["status"] == "to do"],
+        "3": [task for task in current_tasks if task["status"] == "in progress"],
+        "4": [task for task in archived_tasks if task["status"] == "completed"],
+        "5": archived_tasks
+    }
+    task_type = input("""
+1. list current tasks
+2. list to do tasks
+3. list in progress tasks
+4. list completed tasks
+5. list archived tasks
+Choose task type (1/2/3/4/5): """)
+    while task_type not in task_labels_dict:
+        task_type = input("Task type invalid. Try again. Enter task type: ")
+    task_list = task_list_dict[task_type]
+    if not task_list:
+        print("No tasks listed.")
+    else:
+        print("=" * 40)
+        print(f"{task_labels_dict[task_type]:^40}")
+        print("=" * 40)
+        print(f"{"ID":<5} {"Task":<20} {"Status":<12}")
+        print("-" * 40)
+        for task in task_list:
+            print(
+                f"{task["id"]:<5} {format_task_name(task["task"]):<20} {task["status"]:<12}")
+        print()
